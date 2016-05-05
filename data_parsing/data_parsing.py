a28600bced2b59270f4a57242601ef0e65db0e35
Manel <manelbaradad@gmail.com>
2016-05-05 02:00:46 +0200

    Added parsing for million song dataset

--- data_parsing/data_parsing.py
+++ data_parsing/data_parsing.py
@@ -1,8 +1,14 @@
 import sys
 import glob
 import os
+from audioop import add
+import cPickle as pickle
+
+from sphinx.ext.napoleon.iterators import peek_iter
+
 import utils.hdf5_getters as Getters
 import utils.utils as Utils
+import numpy
 
 #reuses code from http://labrosa.ee.columbia.edu/millionsong/sites/default/files/tutorial1.py.txt
 
@@ -11,12 +17,54 @@ class DataParser():
     self.sqliteLocation = sqliteLocation
     self.baseLocation = baseLocation
     self.outDir = outDir
+    self.styles = {}
+    self.pitches_list = []
+    self.timbres_list = []
+    self.tags_list = []
+    self.flushIndex = 0
 
-  def count_files(self):
-   return 'number of song files:' + str(Utils.apply_to_all_files(self.baseLocation))
+  def process_info(self):
+    songs = Utils.apply_to_all_files(self.baseLocation, self.process_h5_file_info, self.flushFunc, 100)
+    f = open(self.outDir + '/styles.save', 'wb')
+    pickle.dump([self.tags_list, self.pitches_list, self.timbres_list], f, protocol=pickle.HIGHEST_PROTOCOL)
+    f.close()
+    return 'number of song files:' + str(songs)
+
+  def process_h5_file_info(self, filename):
+    """
+    This function does 3 simple things:
+    - open the song file
+    - get artist ID and put it
+    - close the file
+    """
+    h5 = Getters.open_h5_file_read(filename)
+    tags = Getters.get_artist_mbtags(h5);
+    if len(tags) == 0:
+      return 0
+    for tag in tags:
+      if tag in self.styles.keys():
+        self.styles[tag] += 1
+      else:
+        self.styles[tag] = 1
+    self.tags_list.append(tags)
+    self.pitches_list.append(Getters.get_segments_pitches(h5))
+    self.timbres_list.append(Getters.get_segments_timbre(h5))
+
+    h5.close()
+    return 1
+
+  def flushFunc(self):
+    f = open(self.outDir + '/obj' + str(self.flushIndex) + '.save', 'wb')
+    pickle.dump([self.tags_list, self.pitches_list, self.timbres_list], f, protocol=pickle.HIGHEST_PROTOCOL)
+    f.close()
+    self.tags_list = []
+    self.pitches_list = []
+    self.timbres_list = []
+    self.flushIndex+=1
 
 
 if __name__ == "__main__":
   kwargs = dict(x.split('=', 1) for x in sys.argv[1:])
   parser = DataParser(kwargs["--sqlite"], kwargs["--basedir"], kwargs["--outDir"])
-  print "Files to read:" + parser.count_files();
+  print "Files to read:" + parser.process_info();
+  print parser.styles
--- data_parsing/utils/utils.py
+++ data_parsing/utils/utils.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-def apply_to_all_files(baseLocation, func=lambda x: x, ext='.h5'):
+def apply_to_all_files(baseLocation, func=lambda x: x, flushFunc=lambda x: x, flushPeriodicity=1000, ext='.h5'):
   """
   From a base directory, go through all subdirectories,
   find all files with the given extension, apply the
@@ -15,12 +15,18 @@ def apply_to_all_files(baseLocation, func=lambda x: x, ext='.h5'):
      number of files
   """
   cnt = 0
+  cnt_no_tags = 0
   # iterate over all files in all subdirectories
   for root, dirs, files in os.walk(baseLocation):
     files = glob.glob(os.path.join(root, '*' + ext))
     # count files
-    cnt += len(files)
+
     # apply function to all files
     for f in files:
-      func(f)
+      cnt+=func(f)
+      cnt_no_tags+=1
+      if cnt%flushPeriodicity == 0:
+        print "Processed" + str(cnt)
+        print "Processed with tags " + str(cnt_no_tags)
+        flushFunc()
   return cnt
