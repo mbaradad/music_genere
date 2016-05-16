d8c332d0d1e9d5e9367d6ef590d69c2fa4a2c1ab
Manel <manelbaradad@gmail.com>
2016-05-16 16:57:25 +0200

    Added preview data parsing

--- data_parsing/utils/mp3_utilities.py
+++ data_parsing/utils/mp3_utilities.py
@@ -4,6 +4,7 @@ from tempfile import mktemp
 from scikits.audiolab import wavread, play
 from scipy.signal import remez, lfilter
 from pylab import *
+import matplotlib.pyplot as plt
 
 from pylab import specgram, show
 import scipy.signal as signal
@@ -13,12 +14,13 @@ def mp3ToDFT(mp3filename):
     # convert mp3, read wav
     wname = mktemp('.wav')
     FNULL = open(os.devnull, 'w')
-    subprocess.call(['avconv', '-i', mp3filename, "-ss", "15", "-t", "3", wname], stdout=FNULL, stderr=subprocess.STDOUT)
+    subprocess.call(['avconv', '-i', mp3filename, "-ss", "15", "-t", "9", wname], stdout=FNULL, stderr=subprocess.STDOUT)
     sig, fs, enc = wavread(wname)
+
     #todo: remove wav file
     #todo: convert to mono, averaging sig[:,0] + sig[;,1]
     os.unlink(wname)
 
-    spectrogram = signal.spectrogram(sig[:,0], fs=fs)
+    spectrogram = signal.spectrogram(sig[:,0], fs=fs)[2][0:62,:]
 
-    print len(spectrogram[2])
+    return spectrogram
