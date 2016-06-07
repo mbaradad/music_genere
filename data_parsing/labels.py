a5b520dc0d443dea6924d229ef403efc335b1424
Manel <manelbaradad@gmail.com>
2016-06-08 01:54:39 +0200

    Added labels get

--- data_parsing/labels.py
+++ data_parsing/labels.py
@@ -2,17 +2,16 @@ import numpy as np
 
 NUMBER_OF_TAGS = 256
 
-example = np.zeros(256);
-example[1] = 0.9
-example[3] = 0.5
-example[10] = 0.3
-
 lines = np.genfromtxt('../styles.csv', delimiter=",", dtype=None)
 styles = list()
 for i in range(NUMBER_OF_TAGS):
   styles.append(lines[i][0])
 
-indices = [i[0] for i in sorted(enumerate(example), key=lambda x:x[1],
-    reverse=True)]
-print(list( styles[i] for i in indices[1:5] ))
-
+a = np.load('../prediction.npz')
+for j in range(0,15):
+  example = a['arr_'+str(j)]
+  if(len(example) != 256): continue
+  values = [i[1] for i in sorted(enumerate(example), key=lambda x:x[1],reverse=True)]
+  indices = [i[0] for i in sorted(enumerate(example), key=lambda x:x[1],reverse=True)]
+  print(list(styles[i] for i in indices[0:10] ))
+  print(list(values[0:]))
