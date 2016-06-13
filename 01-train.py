6fa8617b4a862bd60747f85d34e70c2c7b4f3d20
Manel <manelbaradad@gmail.com>
2016-06-13 21:12:17 +0200

    Changed to sigmoid and added inits

--- 01-train.py
+++ 01-train.py
@@ -16,6 +16,7 @@ from lasagne.layers import SliceLayer, concat, DenseLayer
 
 from lasagne.nonlinearities import softmax, rectify
 from lasagne.utils import floatX
+from lasagne.init import GlorotUniform, Constant
 
 from os import listdir
 from os.path import isfile, join
@@ -31,24 +32,24 @@ def buildNetwork(input_var=None):
     net = {}
 
     # The input shape is (freq, time) -> (130,300)
-    net['input'] = InputLayer((None, 129, 300), input_var=input_var)
+    net['input'] = InputLayer((None, 129, 300), input_var=input_var, W=GlorotUniform('relu'), b=Constant(0.0))
     print "input: {}".format(net['input'].output_shape[1:])
     # conv1
-    net['conv1'] = Conv1DLayer(net['input'], num_filters=256, filter_size=4)
+    net['conv1'] = Conv1DLayer(net['input'], num_filters=256, filter_size=4, W=GlorotUniform('relu'), b=Constant(0.0))
     print "conv1: {}".format(net['conv1'].output_shape[1:])
     # pool1
     net['pool1'] = Pool1DLayer(net['conv1'], pool_size=4)
     print "pool1: {}".format(net['pool1'].output_shape[1:])
 
     # conv2
-    net['conv2'] = Conv1DLayer(net['pool1'], num_filters=256, filter_size=4)
+    net['conv2'] = Conv1DLayer(net['pool1'], num_filters=256, filter_size=4, W=GlorotUniform('relu'), b=Constant(0.0))
     print "conv2: {}".format(net['conv2'].output_shape[1:])
     # pool2
     net['pool2'] = Pool1DLayer(net['conv2'], pool_size=2)
     print "pool2: {}".format(net['pool2'].output_shape[1:])
 
     # conv3
-    net['conv3'] = Conv1DLayer(net['pool2'], num_filters=512, filter_size=4)
+    net['conv3'] = Conv1DLayer(net['pool2'], num_filters=512, filter_size=4, W=GlorotUniform('relu'), b=Constant(0.0))
     print "conv3: {}".format(net['conv3'].output_shape[1:])
 
     # global pool
@@ -63,17 +64,17 @@ def buildNetwork(input_var=None):
 
     # fc6
     net['fc6'] = DenseLayer(net['pool3'], num_units=2048,
-                            nonlinearity=lasagne.nonlinearities.rectify)
+                            nonlinearity=lasagne.nonlinearities.rectify, W=GlorotUniform('relu'), b=Constant(0.0))
     print "fc6: {}".format(net['fc6'].output_shape[1:])
 
     # fc7
     net['fc7'] = DenseLayer(net['fc6'], num_units=2048,
-                            nonlinearity=lasagne.nonlinearities.rectify)
+                            nonlinearity=lasagne.nonlinearities.rectify, W=GlorotUniform('relu'), b=Constant(0.0))
     print "fc7: {}".format(net['fc7'].output_shape[1:])
 
     # output
     net['output'] = DenseLayer(net['fc7'], num_units=100,
-                               nonlinearity=log_softmax)
+                               nonlinearity=lasagne.nonlinearities.sigmoid, W=GlorotUniform('relu'), b=Constant(0.0))
     print "output: {}".format(net['output'].output_shape[1:])
 
     return net
@@ -89,26 +90,37 @@ def batch_gen(X, y, N):
         yield X[idx].astype('float32'), y[idx].astype('int32')
 
 
+
 if __name__ == "__main__":
+
+    # Load data
+
+    # with open( 'validationData.pickle', 'rb') as f:
+    # validationData = pickle.load( f )
+
+    # with open( 'testData.pickle', 'rb') as f:
+    # testData = pickle.load( f )
+
+    # valData = validationData[0:2000]
+
+    # Create network
+
+    # X_train = np.random.randn(1000,12, 300)
+    # y_train = np.random.random_integers(0, 256, 1000)
 
     x = list()
     y = list()
 
-    BATCH_SIZE = 2048
-    numEpochs = 50
-
     inputImage = T.tensor3()
     output = T.imatrix()
+
 
     net = buildNetwork(inputImage)
 
     prediction = lasagne.layers.get_output(net['output'])
     test_prediction = lasagne.layers.get_output(net['output'], deterministic=True)
-    '''
     loss = lasagne.objectives.squared_error(prediction, output)
     # loss = lasagne.objectives.categorical_crossentropy(prediction, output)
-    # loss = lasagne.objectives.binary_crossentropy(prediction, output)
-
     loss = loss.mean()
 
     init_learningrate = 0.01
@@ -129,53 +141,70 @@ if __name__ == "__main__":
 
     predict_fn = theano.function([inputImage], test_prediction)
 
+    BATCH_SIZE = 128
+    numEpochs = 50
+
     # batchIn = np.zeros((batchSize, 12, 300), theano.config.floatX)
     # batchOut = np.zeros((batchSize, 256), theano.config.floatX)
-    '''
     print "loading data..."
-    for f in [f for f in listdir('./data/') if '.npz' in f]:
-        npzfile = np.load('./data/' + f)
+    for f in [f for f in listdir('/home/manel/Downloads') if '.npz' in f]:
+        npzfile = np.load('/home/manel/Downloads/'+ f)
         x.extend(npzfile['x'])
         y.extend(npzfile['y'])
+
+    #x_train = np.random.randint(9,255,size=(190, 129, 300))
+    #y_train = np.random.randint(0,2,size=(190,100))
 
     x_train = np.array(x)
     y_train = np.array(y)
 
     x_train = x_train.transpose(0, 2, 1)
 
-
-    true_output = output
-    loss_train = T.mean(categorical_crossentropy_logdomain(prediction, true_output))
-
-    all_params = lasagne.layers.get_all_params(net['output'])
-    # Use ADADELTA for updates
-    updates = lasagne.updates.adadelta(loss_train, all_params)
-    train_fn = theano.function([net['input'].input_var, true_output], loss_train, updates=updates, allow_input_downcast=True)
-
-    # This is the function we'll use to compute the network's output given an input
-    # (e.g., for computing accuracy).  Again, we don't want to apply dropout here
-    # so we set the deterministic kwarg to True.
-
-    # x_train = np.random.randn(10000, 129, 300)
-    # y_train = np.random.random_integers(0, 1, (10000, 100))
-
     print x_train.shape
     print y_train.shape
-    # print y_train[0]
-    # print x_train[0]
 
     train_batches = batch_gen(x_train, y_train, BATCH_SIZE)
     N_BATCHES = len(x_train) // BATCH_SIZE
 
-    for currEpoch in tqdm(range(numEpochs), ncols=20):
+    # print 'Loading training data...'
+    # with open(pathToImagesPickle, 'rb') as f:
+    #    trainData = pickle.load(f)
+    # print '-->done!'
+
+    for currEpoch in tqdm(range(numEpochs),ncols=20):
+
+        # random.shuffle(trainData)
+        # random.shuffle( valData )
 
         train_err = 0.
+        # val_err = 0.
 
         for _ in range(N_BATCHES):
             X, y = next(train_batches)
             train_err += train_fn(X, y)
+        '''
+        for currChunk in chunks(trainData, batchSize):
 
-        print 'Epoch:', currEpoch, ' ->', train_err/100000 # (train_err, val_err )
+            if len(currChunk) != batchSize:
+                continue
+
+            for k in range(batchSize):
+                batchIn[k, ...] = (currChunk[k].astype(theano.config.floatX) / 255.
+                batchOut[k, ...] = (currChunk[k].output.data.astype(theano.config.floatX))
+
+            train_err += train_fn(batchIn, batchOut)
+
+        for currChunk in chunks(valData, batchSize):
+
+            if len(currChunk) != batchSize:
+                continue
+
+            for k in range( batchSize ):
+                batchIn[k,...] = (currChunk[k].image.data.astype(theano.config.floatX).transpose(2,0,1)-imageMean)/255.
+                batchOut[k,...] = (currChunk[k].saliency.data.astype(theano.config.floatX))/255.
+            val_err += val_fn( batchIn, batchOut )
+        '''
+        print 'Epoch:', currEpoch, ' ->', train_err  # (train_err, val_err )
 
         if currEpoch % 10 == 0:
             np.savez("modelWights{:04d}.npz".format(currEpoch), *lasagne.layers.get_all_param_values(net['output']))
